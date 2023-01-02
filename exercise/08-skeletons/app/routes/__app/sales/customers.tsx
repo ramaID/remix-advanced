3fdb02f24992c822cca029e45062840a55e833de
rama <rama@javan.co.id>
2023-01-02 21:44:48 +0700

    skeletons

--- exercise/08-skeletons/app/routes/__app/sales/customers.tsx
+++ exercise/08-skeletons/app/routes/__app/sales/customers.tsx
@@ -1,9 +1,17 @@
-import { NavLink, Outlet, useLoaderData } from "@remix-run/react";
+import {
+  NavLink,
+  Outlet,
+  useLoaderData,
+  useTransition,
+} from "@remix-run/react";
 import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { FilePlusIcon } from "~/components";
 import { requireUser } from "~/session.server";
 import { getCustomerListItems } from "~/models/customer.server";
+import { useSpinDelay } from "spin-delay";
+
+type LoadingCustomer = Awaited<ReturnType<typeof getCustomerListItems>>[number];
 
 export async function loader({ request }: LoaderArgs) {
   await requireUser(request);
@@ -14,12 +22,16 @@ export async function loader({ request }: LoaderArgs) {
 
 export default function Customers() {
   const { customers } = useLoaderData<typeof loader>();
+  const transition = useTransition();
 
-  // 🐨 get the transition from useTransition
-  // 💰 use transition.location?.state to get the customer we're transitioning to
-
-  // 💯 to avoid a flash of loading state, you can use useSpinDelay
-  // from spin-delay to determine whether to show the skeleton
+  let loadingCustomer: LoadingCustomer | undefined;
+  if (transition.location?.state) {
+    loadingCustomer = (transition.location.state as any)?.customer;
+  }
+  const showSkeleton = useSpinDelay(Boolean(loadingCustomer), {
+    delay: 200,
+    minDuration: 300,
+  });
 
   return (
     <div className="flex overflow-hidden rounded-lg border border-gray-100">
@@ -42,8 +54,7 @@ export default function Customers() {
             <NavLink
               key={customer.id}
               to={customer.id}
-              // 🐨 add state to set the customer for the transition
-              // 💰 state={{ customer }}
+              state={{ customer }}
               prefetch="intent"
               className={({ isActive }) =>
                 "block border-b border-gray-50 py-3 px-4 hover:bg-gray-50" +
@@ -62,12 +73,14 @@ export default function Customers() {
         </div>
       </div>
       <div className="flex w-1/2 flex-col justify-between">
-        {/*
-          🐨 if we're loading a customer, then render the
-          <CustomerSkeleton /> (defined below) instead of
-          the <Outlet />
-        */}
-        <Outlet />
+        {loadingCustomer && showSkeleton ? (
+          <CustomerSkeleton
+            name={loadingCustomer.name}
+            email={loadingCustomer.email}
+          />
+        ) : (
+          <Outlet />
+        )}
         <small className="p-2 text-center">
           Note: this is arbitrarily slow to demonstrate pending UI.
         </small>
